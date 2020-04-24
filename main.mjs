e4e70acf5b61dccfdc0478ace3a27ed113b32a09
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2020-04-25 00:18:01 +0200

    Initial.

---
+++ main.mjs
@@ -0,0 +1,28 @@
+import util from 'util';
+
+import redis from 'redis';
+import createRedisClient from 'redis/lib/createClient';
+
+class _PromiseRedis extends redis.RedisClient {
+    constructor(...args) {
+        if (args.length === 0)
+            args = ['redis://localhost']
+
+        super(createRedisClient(args));
+    }
+}
+
+export default function PromiseRedis(...args) {
+    const client = new _PromiseRedis(...args);
+
+    client.on('error', msg => console.error(msg), null);
+
+    return new Proxy(client, {
+        get(target, key, receiver) {
+            return target.hasOwnProperty(key)
+                ? client[key]
+                : util.promisify(client[key]).bind(client)
+            ;
+        }
+    });
+}
